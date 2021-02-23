6e7ff1247bf35953550540c0162dce256ae600b2
Raumo0 <dharapko@gmail.com>
2021-02-23 21:53:56 +0300

    Added coin turnover amount stats

---
+++ hasura/migrations/1614106341112_alter_table_public_total_tx_hourly_stats_add_column_total_coin_turnover_amount/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."total_tx_hourly_stats" DROP COLUMN "total_coin_turnover_amount";
---
+++ hasura/migrations/1614106341112_alter_table_public_total_tx_hourly_stats_add_column_total_coin_turnover_amount/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."total_tx_hourly_stats" ADD COLUMN "total_coin_turnover_amount" int4 NOT NULL DEFAULT 0;
