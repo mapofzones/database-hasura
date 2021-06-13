c529345bb39bd304073e71e1b50e5907d3589a09
Raumo0 <dharapko@gmail.com>
2021-06-13 17:50:18 +0300

    Add addresses weight to data prepare logic

---
+++ hasura/migrations/1623593533996_alter_table_public_zones_stats_add_column_total_active_addresses_weight/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."zones_stats" ADD COLUMN "total_active_addresses_weight" numeric NOT NULL DEFAULT 0;
---
+++ hasura/migrations/1623594484255_drop_function_with_temp_type/up.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION public.get_full_stats_for_each_zone(integer, integer);
+DROP TYPE IF EXISTS public.temp_t_full_stats_for_each;
