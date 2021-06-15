fd2458701f2e747493771af54ff63977fc3f493a
Raumo0 <dharapko@gmail.com>
2021-06-15 05:01:10 +0300

    Added failed transfers by channel with calculation

---
+++ hasura/migrations/1623714244384_alter_table_public_ibc_transfer_hourly_stats_add_column_txs_fail_cnt/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."ibc_transfer_hourly_stats" DROP COLUMN "txs_fail_cnt";
---
+++ hasura/migrations/1623714244384_alter_table_public_ibc_transfer_hourly_stats_add_column_txs_fail_cnt/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."ibc_transfer_hourly_stats" ADD COLUMN "txs_fail_cnt" integer NOT NULL DEFAULT 0;
