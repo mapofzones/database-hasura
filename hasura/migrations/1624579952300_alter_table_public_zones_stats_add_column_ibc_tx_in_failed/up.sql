cafff214393870c0b521c6fd21a1c1c2b14b0c92
Raumo0 <dharapko@gmail.com>
2021-06-25 03:16:08 +0300

    MOZ-7 Added failed in/out API fields and calculation

---
+++ hasura/migrations/1624579952300_alter_table_public_zones_stats_add_column_ibc_tx_in_failed/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."zones_stats" ADD COLUMN "ibc_tx_in_failed" integer NOT NULL DEFAULT 0;
---
+++ hasura/migrations/1624579970291_alter_table_public_zones_stats_add_column_ibc_tx_out_failed/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."zones_stats" ADD COLUMN "ibc_tx_out_failed" integer NOT NULL DEFAULT 0;
