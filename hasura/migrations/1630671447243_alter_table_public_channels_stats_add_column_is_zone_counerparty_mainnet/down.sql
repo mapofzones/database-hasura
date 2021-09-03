3bf37f2ddf67dd4d31c559411ca2534e6676ea18
Raumo0 <dharapko@gmail.com>
2021-09-03 14:17:53 +0200

    MOZ-60 & MOZ-63 Channels stats update

---
+++ hasura/migrations/1630671447243_alter_table_public_channels_stats_add_column_is_zone_counerparty_mainnet/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."channels_stats" DROP COLUMN "is_zone_counerparty_mainnet";
---
+++ hasura/migrations/1630671447243_alter_table_public_channels_stats_add_column_is_zone_counerparty_mainnet/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."channels_stats" ADD COLUMN "is_zone_counerparty_mainnet" bool NOT NULL DEFAULT false;
