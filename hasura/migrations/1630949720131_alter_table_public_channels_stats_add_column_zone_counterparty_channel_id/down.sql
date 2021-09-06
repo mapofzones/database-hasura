b3c79667646e4aa14304645641c2c4aeef4bd0c2
Raumo0 <dharapko@gmail.com>
2021-09-06 19:37:38 +0200

    MOZ-72 Channel-channel. Added stub flat table column

---
+++ hasura/migrations/1630949720131_alter_table_public_channels_stats_add_column_zone_counterparty_channel_id/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."channels_stats" DROP COLUMN "zone_counterparty_channel_id";
---
+++ hasura/migrations/1630949720131_alter_table_public_channels_stats_add_column_zone_counterparty_channel_id/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."channels_stats" ADD COLUMN "zone_counterparty_channel_id" varchar NULL;
