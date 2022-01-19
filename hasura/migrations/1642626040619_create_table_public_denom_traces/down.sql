fd3f4c581c562b691d5d896f34ca50f970fe7f67
Raumo0 <dharapko@gmail.com>
2022-01-19 22:02:40 +0100

    Added denom_traces core table

---
+++ hasura/migrations/1642626040619_create_table_public_denom_traces/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE "public"."denom_traces";
---
+++ hasura/migrations/1642626040619_create_table_public_denom_traces/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."denom_traces"("hash" varchar NOT NULL, "path" varchar NOT NULL, "base_denom" varchar NOT NULL, PRIMARY KEY ("hash") );
