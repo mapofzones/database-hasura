3c108eb71549460994867238c905de3bf0a17d5b
Raumo0 <dharapko@gmail.com>
2022-01-26 09:13:14 +0100

    Added some nullable values to zones_stats flat table

---
+++ hasura/migrations/1643184516695_ft_zones_stats_added_nullable_fields/down.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE "public"."zones_stats" ALTER COLUMN "ibc_transfers_mainnet_weight" SET NOT NULL;
+
+ALTER TABLE "public"."zones_stats" ALTER COLUMN "ibc_transfers_weight" SET NOT NULL;
