fb378552bc6baddc1aebe19a0dc3e94f3fa10e93
Raumo0 <dharapko@gmail.com>
2022-08-21 00:27:55 +0200

    Base flat schema template added

---
+++ hasura/migrations/1661033779614_base_flat_schema_template_added/down.sql
@@ -0,0 +1,26 @@
+
+DROP TABLE "flat"."blockchain_tf_switched_charts";
+
+DROP TABLE "flat"."blockchain_tf_switched_chart_type";
+
+DROP TABLE "flat"."blockchain_tf_charts";
+
+DROP TABLE "flat"."blockchain_tf_chart_type";
+
+DROP TABLE "flat"."total_tf_switched_charts";
+
+DROP TABLE "flat"."total_tf_switched_chart_type";
+
+DROP TABLE "flat"."channels_stats";
+
+DROP TABLE "flat"."blockchain_switched_stats";
+
+DROP TABLE "flat"."blockchain_stats";
+
+DROP TABLE "flat"."blockchain_relations";
+
+DROP TABLE "flat"."timeframes";
+
+DROP TABLE "flat"."blockchains";
+
+drop schema "flat" cascade;
