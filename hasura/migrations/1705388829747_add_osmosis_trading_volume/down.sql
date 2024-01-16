cb491d198ad0adf0bca7d08708ae56e05c147eda
0xTFM <140174482+0xTFM@users.noreply.github.com>
2024-01-16 10:47:57 +0300

    add osmosis trading volume

---
+++ hasura/migrations/1705388829747_add_osmosis_trading_volume/down.sql
@@ -0,0 +1,233 @@
+ALTER TABLE "public"."token_prices" DROP COLUMN "osmosis_symbol_total_volumes_in_usd";
+
+CREATE OR REPLACE FUNCTION public.get_tokens()
+ RETURNS SETOF temp_t_get_tokens
+ LANGUAGE sql
+ STABLE
+AS $function$
+
+with mainnet_tokens as (
+    SELECT
+        tokens.zone as blockchain,
+        tokens.base_denom as denom,
+        tokens.symbol,
+        tokens.logo_url,
+        symbol_point_exponent
+    FROM
+        public.tokens
+    LEFT JOIN public.zones on tokens.zone = zones.chain_id
+    WHERE
+      is_mainnet = true
+)
+, last_known_price as (
+    SELECT DISTINCT on (zone, base_denom)
+        zone,
+        base_denom,
+        coingecko_symbol_price_in_usd as coingecko,
+        osmosis_symbol_price_in_usd as osmosis
+    FROM
+        public.token_prices
+    WHERE
+        coingecko_symbol_price_in_usd is NOT NULL
+        or osmosis_symbol_price_in_usd is NOT NULL
+    ORDER BY
+        zone,
+        base_denom,
+        datetime DESC
+)
+, last_known_supply as (
+    SELECT DISTINCT on (zone, base_denom)
+        zone,
+        base_denom,
+        nullif(symbol_supply, 0) as supply
+    FROM
+        public.token_prices
+    WHERE
+        symbol_supply is NOT NULL
+    ORDER BY
+        zone,
+        base_denom,
+        datetime DESC
+)
+, prices as (
+    SELECT DISTINCT on (blockchain, denom)
+        blockchain,
+        denom,
+        symbol,
+        logo_url,
+        symbol_point_exponent,
+        coalesce(tp.coingecko_symbol_price_in_usd, tp.osmosis_symbol_price_in_usd, lkp.coingecko, lkp.osmosis, 0) as price,
+        (nullif(coalesce(symbol_supply, supply, 0), 0) * coalesce(tp.coingecko_symbol_price_in_usd, tp.osmosis_symbol_price_in_usd, lkp.coingecko, lkp.osmosis, 0)) / nullif(POW(10, symbol_point_exponent), 0)  as market_cap,
+        nullif(coalesce(symbol_supply, supply, 0), 0) / nullif(POW(10, symbol_point_exponent), 0) as on_chain_supply,
+        coingecko_symbol_total_volumes_in_usd as token_day_trading_volume
+    FROM
+        mainnet_tokens as mt
+    LEFT JOIN public.token_prices as tp on mt.blockchain = tp.zone and mt.denom = tp.base_denom
+    LEFT JOIN last_known_price as lkp on mt.blockchain = lkp.zone and mt.denom = lkp.base_denom
+    LEFT JOIN last_known_supply as lks on mt.blockchain = lks.zone and mt.denom = lks.base_denom
+    WHERE
+      (tp.coingecko_symbol_price_in_usd > 0 or tp.osmosis_symbol_price_in_usd > 0 or lkp.coingecko > 0 or lkp.osmosis > 0)
+      and datetime = (SELECT MAX(datetime) FROM public.token_prices tp2 WHERE tp.zone = tp2.zone and tp.base_denom = tp2.base_denom and tp.datetime = tp2.datetime)
+    ORDER BY
+        blockchain,
+        denom,
+        datetime desc
+)
+, day_ago_prices as (
+    SELECT
+        blockchain,
+        denom,
+        coalesce(coingecko_symbol_price_in_usd, osmosis_symbol_price_in_usd, 0) as price,
+        coingecko_symbol_total_volumes_in_usd as token_day_trading_volume
+    FROM
+        prices
+    LEFT JOIN public.token_prices as tp on prices.blockchain = tp.zone and prices.denom = tp.base_denom
+    WHERE
+        datetime = date_trunc('hour', now() - make_interval(hours => 24))
+)
+, week_ago_prices as (
+    SELECT
+        blockchain,
+        denom,
+        coalesce(coingecko_symbol_price_in_usd, osmosis_symbol_price_in_usd, 0) as price
+    FROM
+        prices
+    LEFT JOIN public.token_prices as tp on prices.blockchain = tp.zone and prices.denom = tp.base_denom
+    WHERE
+        datetime = date_trunc('hour', now() - make_interval(hours => 168))
+)
+, month_ago_prices as (
+    SELECT
+        blockchain,
+        denom,
+        coalesce(coingecko_symbol_price_in_usd, osmosis_symbol_price_in_usd, 0) as price
+    FROM
+        prices
+    LEFT JOIN public.token_prices as tp on prices.blockchain = tp.zone and prices.denom = tp.base_denom
+    WHERE
+        datetime = date_trunc('hour', now() - make_interval(hours => 720))
+)
+
+SELECT
+    prices.blockchain::character varying,
+    prices.denom::character varying,
+    prices.symbol::character varying,
+    prices.logo_url::character varying,
+    prices.price::numeric,
+    (100 * (prices.price - day_ago.price) / nullif(day_ago.price, 0))::numeric as price_day_diff_percent,
+    (100 * (prices.price - week_ago.price) / nullif(week_ago.price, 0))::numeric as price_week_diff_percent,
+    prices.market_cap::numeric,
+    prices.on_chain_supply::numeric,
+    prices.token_day_trading_volume::numeric,
+    (100 * (prices.token_day_trading_volume - day_ago.token_day_trading_volume) / nullif(day_ago.token_day_trading_volume, 0))::numeric as token_day_trading_volume_diff_percent,
+    (100 * (prices.price - month_ago.price) / nullif(month_ago.price, 0))::numeric as price_month_diff_percent
+FROM
+    prices
+    LEFT JOIN day_ago_prices as day_ago on prices.blockchain = day_ago.blockchain and prices.denom = day_ago.denom
+    LEFT JOIN week_ago_prices as week_ago on prices.blockchain = week_ago.blockchain and prices.denom = week_ago.denom
+    LEFT JOIN month_ago_prices as month_ago on prices.blockchain = month_ago.blockchain and prices.denom = month_ago.denom
+
+$function$;
+
+CREATE OR REPLACE FUNCTION public.get_token_charts(request_timestamp timestamp)
+ RETURNS SETOF temp_t_get_token_charts
+ LANGUAGE sql
+ STABLE
+AS $function$
+
+with last_known_price as (
+    SELECT DISTINCT on (zone, base_denom)
+        zone,
+        base_denom,
+        coingecko_symbol_price_in_usd,
+        osmosis_symbol_price_in_usd
+    FROM
+        public.token_prices
+    WHERE
+        coingecko_symbol_price_in_usd is NOT NULL
+        or osmosis_symbol_price_in_usd is NOT NULL
+    ORDER BY
+        zone,
+        base_denom,
+        datetime DESC
+)
+, tokens_dataset as (
+SELECT
+    blockchain,
+    denom,
+    tp.datetime,
+    nullif(coalesce(tp.coingecko_symbol_price_in_usd, tp.osmosis_symbol_price_in_usd, lnp.coingecko_symbol_price_in_usd, lnp.osmosis_symbol_price_in_usd, -1), -1) as price_in_usd,
+    tp.coingecko_symbol_total_volumes_in_usd as volume_in_usd
+FROM
+    flat.tokens as t
+LEFT JOIN public.token_prices as tp ON tp.zone = t.blockchain and tp.base_denom = t.denom
+LEFT JOIN last_known_price as lnp ON lnp.zone = t.blockchain and lnp.base_denom = t.denom
+WHERE
+    tp.datetime > request_timestamp - make_interval(hours => 720/*month*/)
+)
+
+
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'price_monthly'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    price_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+UNION ALL
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'price_weekly'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    price_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+WHERE
+    datetime > request_timestamp - make_interval(hours => 168/*week*/)
+UNION ALL
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'price_daily'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    price_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+WHERE
+    datetime > request_timestamp - make_interval(hours => 24/*day*/)
+UNION ALL
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'volume_monthly'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    volume_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+UNION ALL
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'volume_weekly'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    volume_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+WHERE
+    datetime > request_timestamp - make_interval(hours => 168/*week*/)
+UNION ALL
+SELECT
+    blockchain::character varying as network_id,
+    denom::character varying as denom,
+    'volume_daily'::character varying as chart_type,
+    cast(extract(epoch from datetime) as integer) as point_index,
+    volume_in_usd::numeric as point_value
+FROM
+    tokens_dataset
+WHERE
+    datetime > request_timestamp - make_interval(hours => 24/*day*/)
+
+
+$function$;
